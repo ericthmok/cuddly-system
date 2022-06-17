b6d26d9eac4399eb294c627c07b90646a8893a89
Eric Mok <ericmok@Erics-MacBook-Air.local>
2022-06-17 00:24:36 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+
+const app=express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>
+res.sendFile(path.join(__dirname,'/public/index.html'))
+);
+
+app.get('/notes',(req,res)=>
+res.sendFile(path.join(__dirname,'/public/notes.html'))
+)
+
+app.post('/api/notes',(req,res)=>{
+    console.info(`${req.method} request received to add a new note`)
+    const {title,text}=req.body;
+    if(title&&text){
+        const newNote = {
+            title,
+            text,
+        };
+        fs.readFile('./db/db.json','utf8',(err,data)=>{
+            if(err){
+                console.error(err);
+            }else{
+                const parsedNote=JSON.parse(data);
+                parsedNote.push(newNote);
+                
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNote),
+                    (writeErr)=>
+                    writeErr
+                    ?console.error(writeErr)
+                    :console.info('Successfully updated notes!')
+                );
+            }
+        });
+    }
+    const response = {
+            status:'success',
+            data:req.body,
+        };
+        console.log(response);
+        res.status(201).json(response);
+    });
+
+app.listen(PORT, ()=>
+console.log(`App listening at http://localhost:${PORT}`)
+);
